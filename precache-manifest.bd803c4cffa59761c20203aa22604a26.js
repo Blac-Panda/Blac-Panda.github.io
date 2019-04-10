2bd1328fef26405e38fb3cdd4de6b9c021002bbf
Blac-Panda <oyefesotunmise@gmail.com>
2019-04-10 18:13:41 +0100

    Updates

---
+++ precache-manifest.bd803c4cffa59761c20203aa22604a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/creg/static/media/search.c6a0effb.svg"
+  },
+  {
+    "revision": "494fd8f3dc730a28b9b0",
+    "url": "/creg/static/js/runtime~main.30bd26fe.js"
+  },
+  {
+    "revision": "6b7ec4a52e89b89140fc",
+    "url": "/creg/static/js/main.c5cd026d.chunk.js"
+  },
+  {
+    "revision": "1a04be1bc217ccb4a34f",
+    "url": "/creg/static/js/2.c6a75f39.chunk.js"
+  },
+  {
+    "revision": "6b7ec4a52e89b89140fc",
+    "url": "/creg/static/css/main.9f7eb3a7.chunk.css"
+  },
+  {
+    "revision": "5021d58c9f23d3aad1478f91f0cbf613",
+    "url": "/creg/index.html"
+  }
+];
