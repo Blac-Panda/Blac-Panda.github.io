a53c5db3e70a5c96ce8efe5239e34f2d20db6eb3
Blac-Panda <oyefesotunmise@gmail.com>
2019-04-11 10:50:28 +0100

    -

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AddToHomescreen from 'react-add-to-homescreen'
 
+<AddToHomescreen onAddToHomescreenClick={this.handleAddToHomescreenClick}/>
 
-
+handleAddToHomescreenClick = () => {
+    alert(`
+        1. Open Share menu
+        2. Tap on "Add to Home Screen" button
+    `)
+}
 
 ReactDOM.render(<App/>,  document.getElementById('root'));
 
